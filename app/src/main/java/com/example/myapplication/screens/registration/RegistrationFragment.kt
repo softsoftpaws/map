504d352fa7e62aa34c90e114e619c7ea2e2ec9d6
softsoftpaws <seitnebieva@yandex.ru>
2021-11-01 22:38:25 +0300

    modified according to comments

--- app/src/main/java/com/example/myapplication/screens/registration/RegistrationFragment.kt
+++ app/src/main/java/com/example/myapplication/screens/registration/RegistrationFragment.kt
@@ -15,42 +15,14 @@ import com.example.myapplication.data.User
 import com.example.myapplication.screens.UserViewModel
 import com.google.android.material.textfield.TextInputEditText
 
-class RegistrationFragment:Fragment() {
+class RegistrationFragment : Fragment() {
 
     private lateinit var mUserViewModel: UserViewModel
-
-    private fun validateEmailAddress(mail:String): Boolean {
-        return mail.isNotBlank()
-    }
-
-    private fun validateLog(log:String):Boolean{
-        return log.isNotBlank()
-    }
-
-    private fun validatePass(pass:String, pass2:String): Boolean {
-        return pass.isNotBlank() && pass2.isNotBlank() && pass == pass2
-    }
-
-    private fun validateCheck(): Boolean {
-        return view?.findViewById<Switch>(R.id.switch1)!!.isChecked
-    }
-
-    private fun result(log:String, mail:String, pass:String, pass2:String): Boolean {
-        return validateLog(log) && validateEmailAddress(mail) && validatePass(pass, pass2)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_registration, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
-
 
         view.findViewById<Button>(R.id.regbtn).setOnClickListener {
             val log = view.findViewById<TextInputEditText>(R.id.inputlog).text.toString().trim()
@@ -62,16 +34,42 @@ class RegistrationFragment:Fragment() {
             if (validateCheck() && result(log, mail, pass, pass2)) {
                 val user = User(0, log, mail, pass) //создание объекта
                 mUserViewModel.addUser(user) //добавление в бд
-                Toast.makeText(context,  "Вы зарегистрировались", Toast.LENGTH_LONG).show()
+                Toast.makeText(context, "Вы зарегистрировались", Toast.LENGTH_LONG).show()
                 findNavController().navigate(R.id.action_registrationFragment_to_loginFragment)
 
             } else if (!result(log, mail, pass, pass2)) {
-                Toast.makeText(context,  "Поля заполнены неверно", Toast.LENGTH_LONG).show()
+                Toast.makeText(context, "Поля заполнены неверно", Toast.LENGTH_LONG).show()
 
             } else if (!validateCheck()) {
                 Toast.makeText(context, "Вы не согласились с правилами", Toast.LENGTH_LONG).show()
-
             }
         }
+    }
+
+    private fun validateEmailAddress(mail: String): Boolean {
+        return mail.isNotBlank()
+    }
+
+    private fun validateLog(log: String): Boolean {
+        return log.isNotBlank()
+    }
+
+    private fun validatePass(pass: String, pass2: String): Boolean {
+        return pass.isNotBlank() && pass2.isNotBlank() && pass == pass2
+    }
+
+    private fun validateCheck(): Boolean {
+        return view?.findViewById<Switch>(R.id.switch1)!!.isChecked
+    }
+
+    private fun result(log: String, mail: String, pass: String, pass2: String): Boolean {
+        return validateLog(log) && validateEmailAddress(mail) && validatePass(pass, pass2)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_registration, container, false)
     }
 }
