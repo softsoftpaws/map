214efe879893cc33f04dd87801c852096815a1fc
softsoftpaws <seitnebieva@yandex.ru>
2021-11-04 14:41:54 +0300

    рабочий нав бар

--- app/src/main/java/com/example/myapplication/MapActivity.kt
+++ app/src/main/java/com/example/myapplication/MapActivity.kt
@@ -1,25 +1,30 @@
 package com.example.myapplication
 
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MapActivity : AppCompatActivity() {
-
-    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        sharedPreferences = getSharedPreferences("SHARED_PREFERENCES", Context.MODE_PRIVATE)
+//        Room.databaseBuilder(applicationContext, PlaceDatabase::class.java, "place_table")
+//            .addMigrations(PlaceDatabase.MIGRATION_1_2).build()
 
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-    }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerViewMap) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.mapsFragment, R.id.markerFragment, R.id.profileFragment))
 
-    private fun deleteData() {
-        val editor: SharedPreferences.Editor = sharedPreferences.edit()
-        editor.clear().apply()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
