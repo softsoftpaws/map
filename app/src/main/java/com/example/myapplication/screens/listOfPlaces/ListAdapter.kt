1e7fccf7ac5af0a8e9589f9a4860e8dce8137872
softsoftpaws <seitnebieva@yandex.ru>
2021-11-11 20:24:41 +0300

    добавлен переход при нажатии на элемент в списке мест в информацию о месте

--- app/src/main/java/com/example/myapplication/screens/listOfPlaces/ListAdapter.kt
+++ app/src/main/java/com/example/myapplication/screens/listOfPlaces/ListAdapter.kt
@@ -8,24 +8,49 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 import com.example.myapplication.data.placeData.Place
 
-class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+class ListAdapter : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
 
+
+    var onItemClick: ((Place) -> Unit)? = null
+
+//    private lateinit var mListener:onItemClickListener
+//
     override fun getItemCount() = placeList.size
-
     private var placeList = emptyList<Place>()
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {}
+//    interface onItemClickListener{
+//        fun onItemClick(position:Int)
+//    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(LayoutInflater.from(parent.context)
-            .inflate(R.layout.custom_row, parent, false))
+
+//    fun setOnItemClickListener(listener: onItemClickListener){
+//        mListener = listener
+//    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+        init {
+            itemView.setOnClickListener {
+                onItemClick?.invoke(placeList[adapterPosition])
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.custom_row, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         val currentItem = placeList[position]
-        holder.itemView.findViewById<TextView>(R.id.id_txt).text = currentItem.id.toString()
+
+        holder.itemView.findViewById<TextView>(R.id.id_txt).text = (position+1).toString()
         holder.itemView.findViewById<TextView>(R.id.placeName_txt).text = currentItem.object_name
         holder.itemView.findViewById<TextView>(R.id.placeType_txt).text = currentItem.object_type
+
     }
 
     fun setData(place: List<Place>) {
@@ -33,3 +58,8 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
         notifyDataSetChanged() // адаптер рисует все элементы списка
     }
 }
+
+
+
+
+
--- app/src/main/java/com/example/myapplication/screens/listOfPlaces/PlaceListFragment.kt
+++ app/src/main/java/com/example/myapplication/screens/listOfPlaces/PlaceListFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
@@ -25,10 +26,28 @@ class PlaceListFragment : Fragment() {
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerview)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
         mMapViewModel = ViewModelProvider(this).get(MapViewModel::class.java)
         mMapViewModel.readAllData.observe(viewLifecycleOwner, Observer { place ->
             adapter.setData(place)
         })
+//        adapter.setOnItemClickListener(object : ListAdapter.onItemClickListener{
+//            override fun onItemClick(position: Int) {
+//                val clickItem = position
+
+
+
+        adapter.onItemClick = {currentItem->
+            val placeName = currentItem.object_name
+            findNavController().navigate(PlaceListFragmentDirections.actionPlacesFragmentToPlaceFragment(
+                placeName))
+        }
+
+
+
+
+//                findNavController().navigate(R.id.action_placesFragment_to_placeFragment)
+
 
         return view
     }
